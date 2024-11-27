a4936dd3dfd0ebf045d215c37d2ef147f46c8c31
williamj <williamj949@gmail.com>
2024-11-27 20:25:27 +0530

    Downgrade coreKTX and kotlin deps version, add camera screen and take picture functionality

--- androidApp/src/main/java/com/sublime/visionaid/android/MainActivity.kt
+++ androidApp/src/main/java/com/sublime/visionaid/android/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.sublime.visionaid.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.sublime.visionaid.Greeting
+import com.sublime.visionaid.android.ui.CameraScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,24 +17,20 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    GreetingView(Greeting().greet())
+                    CameraScreen(
+                        onCaptureImage = { uri ->
+                            // You can now pass this to your Azure Vision services
+                            Log.e("MainActivity", uri.toString())
+                        },
+                        onCaptureError = { error ->
+                            // Handle any errors that occur during camera operation
+                            Log.e("MainActivity", error)
+                        },
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
