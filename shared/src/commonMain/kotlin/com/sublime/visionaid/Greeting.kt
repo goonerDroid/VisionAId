11c795d4be2db464b3680d08aa30b26db2f80a3b
williamj <williamj949@gmail.com>
2024-11-27 19:56:58 +0530

    Intial Commit

---
+++ shared/src/commonMain/kotlin/com/sublime/visionaid/Greeting.kt
@@ -0,0 +1,9 @@
+package com.sublime.visionaid
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
